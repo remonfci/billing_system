7f25d88070554e330ee616976053cc536fab9de7
Remon Adel <rad@ordbogen.com>
2018-05-02 16:35:00 +0200

    write unit testing for invoice service

---
+++ tests/Domain/Service/invoice-service.spec.php
@@ -0,0 +1,8 @@
+<?php
+
+describe('InvoiceFactory', function () {
+    describe('->generateInvoices()', function () {
+        it('should query the repository for uninvoiced Orders');
+        it('should return an Invoice for each uninvoiced Order');
+    });
+});
